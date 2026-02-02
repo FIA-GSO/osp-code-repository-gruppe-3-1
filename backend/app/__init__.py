1a3586b09dee0c77ba2f7a8b521eb716441b3fca
Pravin <pravin.g2@gso.schule.koeln>
2026-02-02 10:56:13 +0100

    SMTP mit GMAIL implementiert

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -34,7 +34,7 @@ def create_app():
     from app.routes.user_routes import api as user_ns
     api.add_namespace(user_ns, path="/api/users")
     
-    from backend.app.routes.smtp.mail_routes import mail_bp
+    from app.routes.smtp.mail_routes import mail_bp
     app.register_blueprint(mail_bp, url_prefix='/mail')
 
     return app
--- backend/app/config.py
+++ backend/app/config.py
@@ -9,9 +9,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = os.getenv('MAIL_PORT')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', "true").lower() =="true"
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', "false").lower()=="true"
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
